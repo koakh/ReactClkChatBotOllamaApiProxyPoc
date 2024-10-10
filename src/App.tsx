df82590f0696bea6b71a5d941838f5ea264ab834
mariomonteiro <mario.monteiro@critical-links.com>
2024-10-10 12:12:06 +0100

    styling and work no  newChat, start to move to nextMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [history, setHistory] = useState<{ type: 'server' | 'user'; prompt: string; timestamp: number }[]>([]);
+  const [history, setHistory] = useState<{ type: 'assistant' | 'user'; prompt: string; timestamp: number }[]>([]);
   const [context, setContext] = useState([]);
-  const [prompt, setPrompt] = useState('');
-  const [system, setSystem] = useState('You are a helpful assistant.');
+  const [system, setSystem] = useState('You are a helpful assistant. please response with one or two sentence only');
+  const [title, setTitle] = useState('New chat title');
+  const [prompt, setPrompt] = useState('can you explain me the RGB colors scheme?');
+  const [newChat, setNewChat] = useState(true);
   const [loading, setLoading] = useState(false);
   const [feedback, setFeedback] = useState<{ [key: number]: 'up' | 'down' }>({});
   const [editableIndex, setEditableIndex] = useState<number | null>(null);
   const [editableText, setEditableText] = useState('');
   const [selectedTag, setSelectedTag] = useState('');
-  const [tags, setTags] = useState<{name: string}[]>([]);
+  // TODO: full object props
+  const [tags, setTags] = useState<{ name: string }[]>([]);
 
-  // const apiUrl = 'http://c3edu.online:11434/api';
-  // const apiUrl = 'http://c3edu.online/backend/v1/micropal';
-  // const apiEndpointTags = `http://c3edu.online:11434/api/tags`;
-  const apiEndpointTags = `http://c3edu.online/backend/v1/micropal/chat/ollama-tags`;
-  const apiEndpointGenerate = `http://c3edu.online:11434/api/generate`;
-  
+  const baseApiUrl = 'https://c3edu.online/backend/v1/micropal/chat';
+  const apiEndpointTags = `${baseApiUrl}/ollama-tags`;
+  const apiEndpointNewChat = `${baseApiUrl}/new`;
+  // TODO:
+  // const apiEndpointnextMessage = `${baseApiUrl}/next-message`;
+  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRldiIsInN1YiI6IkNOPWRldixPVT1DM0RldmVsb3BlcixPVT1QZW9wbGUsREM9YzNlZHUsREM9b25saW5lIiwicm9sZXMiOlsiQzNfREVWRUxPUEVSIiwiQzNfQURNSU5JU1RSQVRPUiJdLCJwZXJtaXNzaW9ucyI6WyJSUF9BQ1RJVkVfRElSRUNUT1JZIiwiUlBfQU5BTFlUSUNTIiwiUlBfQVBQUyIsIlJQX0FVRElUIiwiUlBfQkFORFdJRFRIX0xJTUlUUyIsIlJQX0JBVFRFUlkiLCJSUF9CTEFDS19MSVNUSU5HIiwiUlBfQ0FDSElORyIsIlJQX0NMQVNTRVMiLCJSUF9DT05URU5UIiwiUlBfREFTSEJPQVJEIiwiUlBfRklSRVdBTEwiLCJSUF9HUE8iLCJSUF9JTlRFUk5FVF9BQ0NFU1MiLCJSUF9LSU9TSyIsIlJQX0xBTkRJTkdfUEFHRSIsIlJQX0xFQVJOSU5HX1BBVEhTIiwiUlBfTElDRU5TRSIsIlJQX0xPQ0FMX0FSRUFfTkVUV09SSyIsIlJQX01BSU5URU5BTkNFIiwiUlBfTUlDUk9QQUxfQ0hBVCIsIlJQX01JQ1JPUEFMX0hJU1RPUlkiLCJSUF9NSUNST1BBTF9UT09MUyIsIlJQX01PREVNIiwiUlBfTU9OSVRPUklORyIsIlJQX1BST1hZX1NFVFRJTkdTIiwiUlBfUkVNT1RFX1NFUlZJQ0VTIiwiUlBfU0hBUkVTIiwiUlBfVElNRV9DT05GSUdVUkFUSU9OIiwiUlBfVVBEQVRFUiIsIlJQX1VTRVJTIiwiUlBfV0hJVEVMSVNUSU5HIiwiUlBfV0lSRUxFU1MiLCJSUF9XSVJFTEVTU19BQ0NFU1MiXSwibWV0YURhdGEiOnsicHJvZmlsZSI6IkMzRGV2ZWxvcGVyIn0sImlhdCI6MTcyODQ5MzM1NiwiZXhwIjozMzI4NjA5MzM1Nn0.aYB4MK94latLr3rWRw-BmF7wMjxGACacCfT1EMNDugY';
+  const headers = useMemo(() => {
+    return {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    };
+  }, []);
+
   const handleCancel = () => {
     if (editableIndex !== null) {
       setEditableText(history[editableIndex]?.prompt || '');
@@ -47,20 +58,21 @@ function App() {
     }
   };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const sendPrompt = async () => {
+
+  const sendPrompt = useCallback(async () => {
     setLoading(true);
 
-    let tempHistory = [...history, { prompt: "", type: 'server' as 'server', timestamp: Date.now() }];
+    let tempHistory = [...history, { prompt: '', type: 'assistant' as 'assistant', timestamp: Date.now() }];
 
     setHistory(tempHistory);
     const tempIndex = tempHistory.length - 1;
 
     const requestOptions = {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers,
       body: JSON.stringify({
         model: selectedTag,
+        title,
         prompt,
         system,
         template: '',
@@ -68,17 +80,18 @@ function App() {
         options: { temperature: 0.8 }
       })
     };
-
-    const response = await fetch(apiEndpointGenerate, requestOptions);
+    const response = await fetch(apiEndpointNewChat, requestOptions);
     const reader = response.body?.getReader();
 
     if (reader) {
-      let serverResponse = '';
+      let assistantResponse = '';
 
       while (true) {
         const { value, done } = await reader.read();
         if (done) {
           setLoading(false);
+          setNewChat(false);
+          setPrompt('');
           break;
         }
 
@@ -89,8 +102,8 @@ function App() {
           const { response, done, context } = JSON.parse(decodedValue);
 
           if (response) {
-            serverResponse += response;
-            tempHistory[tempIndex].prompt = serverResponse;
+            assistantResponse += response;
+            tempHistory[tempIndex].prompt = assistantResponse;
             setHistory([...tempHistory]);
           }
 
@@ -102,15 +115,18 @@ function App() {
         }
       }
     }
-  };
+  }, [apiEndpointNewChat, context, headers, history, prompt, selectedTag, system, title]);
 
   useEffect(() => {
-    fetch(apiEndpointTags)
-    .then((response) => response.json())
-    .then((data: {models: any[]}) => {
-      setTags(data.models);
-    });
-  }, [apiEndpointTags]);
+    fetch(apiEndpointTags, { headers })
+      .then((response) => response.json())
+      .then((data: { models: any[] }) => {
+        setTags(data.models);
+        if (data?.models[0]?.name) {
+          setSelectedTag(data.models[0].name);
+        }
+      });
+  }, [apiEndpointTags, headers]);
 
   useEffect(() => {
     // if latest message is from the user, call sendPrompt
@@ -131,21 +147,21 @@ function App() {
                 <textarea
                   className="textarea-editable"
                   value={editableText}
-                  onChange={(e) => setEditableText(e.target.value)}                  
+                  onChange={(e) => setEditableText(e.target.value)}
                 />
               ) : null}
-              {item.type === 'server' && (
+              {item.type === 'assistant' && (
                 <div className="feedback-icons">
-                   {editableIndex === index && (
-                  <>
-                    <button className="saveBtn" onClick={handleSave}>Save</button>
-                    <button className="cancelBtn" onClick={handleCancel}>Cancel</button>
-                  </>
-                )}
-                  <span><button className={`${feedback[index] === 'up' ? 'selected' :''}`} onClick={() => handleFeedback(index, 'up')}>👍</button>
-                    <button className={`${feedback[index] === 'down' ? 'selected' :''}`} onClick={() => handleFeedback(index, 'down')}>👎</button>
+                  {editableIndex === index && (
+                    <>
+                      <button className="saveBtn" onClick={handleSave}>Save</button>
+                      <button className="cancelBtn" onClick={handleCancel}>Cancel</button>
+                    </>
+                  )}
+                  <span><button className={`${feedback[index] === 'up' ? 'selected' : ''}`} onClick={() => handleFeedback(index, 'up')}>👍</button>
+                    <button className={`${feedback[index] === 'down' ? 'selected' : ''}`} onClick={() => handleFeedback(index, 'down')}>👎</button>
                   </span>
-                 
+
                 </div>
               )}
             </div>
@@ -154,6 +170,14 @@ function App() {
       </div>
 
       <div className="input-area">
+        {newChat && <textarea
+          className="textarea"
+          placeholder="System prompt (optional)"
+          value={system}
+          onChange={(e) => setSystem(e.target.value)}
+        ></textarea>
+        }
+
         <textarea
           className="textarea"
           placeholder="Enter your prompt"
@@ -161,30 +185,30 @@ function App() {
           onChange={(e) => setPrompt(e.target.value)}
         ></textarea>
 
-        <textarea
-          className="textarea"
-          placeholder="System prompt (optional)"
-          value={system}
-          onChange={(e) => setSystem(e.target.value)}
-        ></textarea>
-        <div>
-
-        <select onChange={(v) => setSelectedTag(v.target.value)} >
-          {tags.map((tag) => (
-            <option key={tag.name} value={tag.name}>
-              {tag.name}
-            </option>
-          ))}
-        </select>
-                <button
-          className={`send-button ${loading ? 'disabled' : ''}`}
-          disabled={loading}
-          onClick={async () => {
-            setHistory(prevHistory => [...prevHistory, { prompt, type: 'user', timestamp: Date.now() }])
-          }}
-        >
-          Send
-        </button>
+        <div className='toolbox'>
+          {newChat &&
+            <><input
+              type='text'
+              placeholder="New chat title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            ></input><select onChange={(v) => setSelectedTag(v.target.value)}>
+                {tags?.map((tag) => (
+                  <option key={tag.name} value={tag.name}>
+                    {tag.name}
+                  </option>
+                ))}
+              </select><button
+                className={`send-button ${loading ? 'disabled' : ''}`}
+                style={{ marginLeft: "auto" }}
+                disabled={loading && selectedTag !== ''}
+                onClick={async () => {
+                  setHistory(prevHistory => [...prevHistory, { prompt, type: 'user', timestamp: Date.now() }]);
+                }}
+              >
+                Send
+              </button></>
+          }
         </div>
       </div>
     </div>
